146b6c18e3f699eec8b1cff24b91b6f91d905954
Dinis <cruzdinis@hotmail.com>
2021-05-10 14:38:24 +0100

    Added scripts folder

--- src/test/java/tqs/projet/airquality/AirQualityControllerTests.java
+++ src/test/java/tqs/projet/airquality/AirQualityControllerTests.java
@@ -1,4 +1,5 @@
 package tqs.projet.airquality;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
